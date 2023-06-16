e0ba96f88f536f33f6fbd8f1feb29ccb4c3425d4
kaushman9 <patelkaush99@gmail.com>
2023-06-15 23:59:25 -0400

    what message

---
+++ file3.js
@@ -0,0 +1,55 @@
+var stripe = Stripe('pk_test_TYooMQauvdEDq54NiTphI7jx'); // Use your test API key here
+var elements = stripe.elements();
+
+// Create an instance of the card Element
+var card = elements.create('card');
+
+// Add an instance of the card Element into the `card-element` <div>
+card.mount('#card-element');
+
+// Handle real-time validation errors from the card Element.
+card.addEventListener('change', function(event) {
+  var displayError = document.getElementById('card-errors');
+  if (event.error) {
+    displayError.textContent = event.error.message;
+  } else {
+    displayError.textContent = '';
+  }
+});
+
+// Handle form submission
+var form = document.getElementById('payment-form');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  stripe.createToken(card).then(function(result) {
+    if (result.error) {
+      // Inform the user if there was an error
+      var errorElement = document.getElementById('card-errors');
+      errorElement.textContent = result.error.message;
+    } else {
+      // Add additional card details to token
+      result.token.name = document.getElementById('name').value;
+      result.token.email = document.getElementById('email').value;
+      result.token.address_line1 = document.getElementById('address').value;
+      result.token.billing_address = document.getElementById('billing-address').value;
+
+      // Send the token to your server
+      stripeTokenHandler(result.token);
+    }
+  });
+});
+
+// Submit the form with the token ID
+function stripeTokenHandler(token) {
+  // Insert the token ID into the form so it gets submitted to the server
+  var form = document.getElementById('payment-form');
+  var hiddenInput = document.createElement('input');
+  hiddenInput.setAttribute('type', 'hidden');
+  hiddenInput.setAttribute('name', 'stripeToken');
+  hiddenInput.setAttribute('value', token.id);
+  form.appendChild(hiddenInput);
+
+  // Submit the form
+  form.submit();
+}
